164f770e834d4682a955536d52325bd4b5982f71
Kartikeya Kesarwani <163706267+kartikkes02@users.noreply.github.com>
2025-04-21 19:47:50 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,136 @@
+gsap.from(".head1",{
+    x:-750,
+    duration:4,
+    delay:2
+})
+gsap.from(".head2",{
+    y:-700,
+    duration:4,
+    delay:1
+})
+gsap.from(".hh",{
+    x:-700,
+    y:-500,
+    duration:3.5,
+    delay:1
+})
+gsap.from(".h4i",{
+    x:-700,
+    duration:2,
+    delay:1,
+    scrollTrigger:{
+        scroll:".h4i",
+        trigger:"body",
+        start:"bottom 90%"
+    }
+})
+gsap.from(".h4ii",{
+    x:800,
+    duration:2,
+    delay:1,
+    scrollTrigger:{
+        scroll:".h4ii",
+        trigger:"body",
+        start:"bottom 90%"
+    }
+},"a")
+gsap.from(".h5i",{
+    x:-700,
+    duration:3,
+    delay:1,
+    scrollTrigger:{
+        scroller: "body",
+        trigger:".h5i",
+        start:"bottom 88%"
+    }
+})
+gsap.from(".h5ii",{
+    x:-700,
+    duration:3,
+    delay:1,
+    scrollTrigger:{
+        scroller:"body",
+        trigger:".hrii",
+        start:"bottom 85%"
+    }
+},"a")
+gsap.from(".h5iii",{
+    x:-700,
+    duration:3,
+    delay:1,
+    scrollTrigger:{
+        trigger:".h5iii",
+        scroller:"body",
+        start:"bottom 83%"
+    }
+}) 
+gsap.from(".h5iv",{
+    x:-700,
+    duration:3,
+    delay:1,
+    scrollTrigger:{
+        trigger:".h5iv",
+        scroller:"body",
+        start:"bottom 81%"
+    }
+})
+gsap.from(".h5v",{
+    x:-700,
+    duration:3,
+    delay:1,
+    scrollTrigger:{
+        trigger:".h5v",
+        scroller:"body",
+        start:"bottom 79%"
+    }
+})
+gsap.from(".h5vi",{
+    x:-600,
+    duration:3,
+    delay:1,
+    scrollTrigger:{
+        trigger:".h5vi",
+        scroller:"body",
+        start:"bottom 77%"
+    }
+})
+gsap.from(".h5vii",{
+    x:-700,
+    duration:3,
+    delay:1,
+    scrollTrigger:{
+        trigger:".h5vii",
+        scroller:"body",
+        start:"bottom 75%"
+    }
+})
+gsap.from(".h6i",{
+    x:-700,
+    duration:3,
+    delay:1,
+    scrollTrigger:{
+        trigger:".h6i",
+        scroller:"body",
+        start:"bottom 72%"
+    }
+})
+gsap.from(".h6ii",{
+    x:-600,
+    duration:3,
+    delay:1,
+    scrollTrigger:{
+        trigger:".h6ii",
+        scroller:"body",
+        start:"bottom 72%"
+    }
+})
+gsap.from(".h6iii",{
+    x:-700,
+    duration:3,
+    delay:1,
+    scrollTrigger:{
+        trigger:".h6iii",
+        scroller:"body",
+        start:"bottom 72%"
+    }
+}) 
